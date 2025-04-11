2e4d6ff71cc421c3b21cd62f77b11d8c7fa676d3
jaymenigri <jaymenigri@gmail.com>
2025-04-11 16:58:29 +0300

    Update detectLanguage.js

--- utils/detectLanguage.js
+++ utils/detectLanguage.js
@@ -1,14 +1,13 @@
 let franc = require('franc-min');
 const langs = require('langs');
 
-// Corrige caso Heroku importe franc-min como objeto
 if (typeof franc !== 'function' && typeof franc.default === 'function') {
   franc = franc.default;
 }
 
 function detectLanguage(text) {
   const langCode = franc(text);
-  if (langCode === 'und') return 'en'; // fallback para inglês
+  if (langCode === 'und') return 'en';
   const language = langs.where("3", langCode);
   return language ? language['1'] : 'en';
 }
