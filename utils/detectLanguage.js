667014043e64186a036637c6e4068c44e7b46852
jaymenigri <jaymenigri@gmail.com>
2025-04-11 17:33:56 +0300

    Create again detectLanguage.js

---
+++ utils/detectLanguage.js
@@ -0,0 +1,15 @@
+let franc = require('franc-min');
+const langs = require('langs');
+
+if (typeof franc !== 'function' && franc.default && typeof franc.default === 'function') {
+  franc = franc.default;
+}
+
+function detectLanguage(text) {
+  const langCode = franc(text);
+  if (langCode === 'und') return 'en';
+  const language = langs.where("3", langCode);
+  return language ? language['1'] : 'en';
+}
+
+module.exports = detectLanguage;
